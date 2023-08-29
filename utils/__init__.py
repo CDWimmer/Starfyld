144ac45df757db2021fd1c78041ca2ee8e193b37
CDWimmer <31413434+CDWimmer@users.noreply.github.com>
2023-08-29 12:10:47 +0100

    Initial commit

---
+++ utils/__init__.py
@@ -0,0 +1,19 @@
+from random import randint
+
+from pyglet import shapes
+
+
+def rand_color() -> tuple:
+    return randint(0, 255), randint(0, 255), randint(0, 255), randint(150, 255)
+
+
+def make_random_circle(left, right, top, bottom, batch_name):
+    """Return a random circle within the bounds given"""
+    # print(left, right, top, bottom)
+    return shapes.Circle(
+        x=randint(*sorted([left, right])),
+        y=randint(*sorted([bottom, top])),
+        radius=randint(1, 10),
+        color=rand_color(),
+        batch=batch_name
+    )
